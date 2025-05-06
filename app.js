520db2f776c909b2ac7ee0120d5e51c2857c9dda
Hassan <hassanmousavi400@gmail.com>
2025-05-06 22:34:54 +0330

    part2_js

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 const resultInput = document.querySelector("result");
 const calcInput = document.querySelector(".calculation");
 const removeBtn = document.querySelector(".backcolor_remove");
-const operatorBtn = document.querySelectorAll(".backcolor_operator");
-const numberBtn = document.querySelectorAll(".num_btn");
-const equalBtn = document.querySelector(".equal_btn");
 const display = document.getElementById("display");
 const result = document.getElementById("result");
+// operators
+const clearBtn = document.querySelector(".clear_btn");
+const percentBtn = document.querySelector(".percent_btn");
+const dividBtn = document.querySelector(".divid_btn");
+const multipleBtn = document.querySelector(".multiple_btn");
+const plusBtn = document.querySelector(".plus_btn");
+const minusBtn = document.querySelector(".minus_btn");
+const equalBtn = document.querySelector(".equal_btn");
 
 function appendToDisplay(value) {
   display.value += value;
 }
-operatorBtn.forEach((e) => {
-  e.addEventListener("click", function () {
-    calcInput.value = e;
-  });
+
+clearBtn.addEventListener("click", function () {
+  calcInput.value = "";
+});
+
+percentBtn.addEventListener("click", function () {
+  calcInput.value += "%";
+});
+
+dividBtn.addEventListener("click", function () {
+  calcInput.value += "÷";
+});
+
+multipleBtn.addEventListener("click", function () {
+  calcInput.value += "×";
+});
+
+plusBtn.addEventListener("click", function () {
+  calcInput.value += "+";
+});
+
+minusBtn.addEventListener("click", function () {
+  calcInput.value += "-";
+});
+// Calculation values
+equalBtn.addEventListener("click", function () {
+  if (calcInput.append("%")) {
+    calcInput %= calcInput;
+  }
+  console.log(calcInput.value);
 });
